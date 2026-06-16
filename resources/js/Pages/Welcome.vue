78fac8513b30177efc4fff417a14bc4552decb2c
Doneville <donevillek12@gmail.com>
2026-06-16 13:06:28 +0200

    contribution

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -61,7 +61,7 @@ const features = [
     },
     {
         icon: 'M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z',
-        nl: 'Veilig & betrouwbaar', en: 'Safe & reliable', pt: 'Seguro e confiável', es: 'Seguro y fiable',
+        nl: 'Betrouwbaar & Veilig', en: 'Safe & reliable', pt: 'Seguro e confiável', es: 'Seguro y fiable',
         desc_nl: 'Certificeerde luchtvaartstandaarden', desc_en: 'Certified aviation standards', desc_pt: 'Padrões de aviação certificados', desc_es: 'Estándares de aviación certificados',
     },
 ]
